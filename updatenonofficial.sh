359025fbbf7fd575e393fef8d36187141e13c936
ezlabsnodes <ezlabsnodes@gmail.com>
2025-07-12 12:09:14 +0700

    Create updatenonofficial.sh

---
+++ updatenonofficial.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Create directory 'ezlabs'
+mkdir -p ezlabs
+
+# Copy files to 'ezlabs'
+cp /root/rl-swarm/modal-login/temp-data/userApiKey.json /root/ezlabs/
+cp /root/rl-swarm/modal-login/temp-data/userData.json /root/ezlabs/
+cp /root/rl-swarm/swarm.pem /root/ezlabs/
+
+# Close Screen and Remove Old Repository
+screen -XS gensyn quit && rm -rf nonofficialauto.zip && cd ~ && rm -rf rl-swarm
+
+# Install Automation Tools
+sudo apt-get update
+sudo apt-get install expect -y
+
+# Download and Unzip ezlabs7.zip, then change to rl-swarm directory
+sudo apt-get install -y unzip && \
+wget https://github.com/ezlabsnodes/gensyn/raw/refs/heads/main/nonofficialauto.zip && \
+unzip nonofficialauto.zip && \
+cd rl-swarm
+
+# Copy swarm.pem to /root/rl-swarm/
+cp /root/ezlabs/swarm.pem /root/rl-swarm/
+
+# Create Screen and run commands
+screen -S gensyn -dm bash -c "python3 -m venv .venv && source .venv/bin/activate && chmod +x run_rl_swarm.sh && ./run_rl_swarm.sh"
+
+echo "Script completed. The 'gensyn' screen session should be running in the background."
+echo "Check logs : tail -f /root/rl-swarm/logs/swarm_launcher.log"
