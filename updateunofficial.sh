916b540253eaf5f365909b1f41e474998642d42c
ezlabsnodes <ezlabsnodes@gmail.com>
2025-07-13 14:49:12 +0700

    Update updateunofficial.sh

--- updateunofficial.sh
+++ updateunofficial.sh
@@ -20,7 +20,7 @@ sudo apt-get install expect -y
 sudo apt-get install -y unzip && \
 wget https://github.com/ezlabsnodes/gensyn/raw/refs/heads/main/nonofficialauto.zip && \
 unzip nonofficialauto.zip && \
-cd rl-swarm
+cd ~/rl-swarm
 
 # Copy swarm.pem to $HOME/rl-swarm/
 cp $HOME/ezlabs/swarm.pem $HOME/rl-swarm/
