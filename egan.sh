375f2588e28f878b6f99399d22be0e1d8fe5873d
ezlabsnodes <ezlabsnodes@gmail.com>
2025-07-25 21:35:32 +0700

    Create egan.sh

---
+++ egan.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create directory 'ezlabs'
+mkdir -p ezlabs
+
+# Copy files to 'ezlabs'
+cp $HOME/rl-swarm/modal-login/temp-data/userApiKey.json $HOME/ezlabs/
+cp $HOME/rl-swarm/modal-login/temp-data/userData.json $HOME/ezlabs/
+cp $HOME/rl-swarm/swarm.pem $HOME/ezlabs/
+
+screen -XS gensyn quit
+
+# Change to rl-swarm directory
+cd ~/rl-swarm
+
+# Copy swarm.pem to $HOME/rl-swarm/
+cp $HOME/ezlabs/swarm.pem $HOME/rl-swarm/
+
+# Create Screen and run commands
+screen -S gensyn -dm bash -c "source .venv/bin/activate && chmod +x run_rl_swarm.sh && ./run_rl_swarm.sh"
+
+echo "Script completed. The 'gensyn' screen session should be running in the background."
+echo "Check logs : tail -f $HOME/rl-swarm/logs/swarm_launcher.log"
