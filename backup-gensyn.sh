25088c18cd4b344be7273601e53178ca2cc30bca
ezlabsnodes <ezlabsnodes@gmail.com>
2025-07-28 05:48:07 +0700

    Create backup-gensyn.sh

---
+++ backup-gensyn.sh
@@ -0,0 +1,7 @@
+# Create directory 'ezlabs'
+mkdir -p ezlabs
+
+# Copy files to 'ezlabs'
+cp $HOME/rl-swarm/modal-login/temp-data/userApiKey.json $HOME/ezlabs/
+cp $HOME/rl-swarm/modal-login/temp-data/userData.json $HOME/ezlabs/
+cp $HOME/rl-swarm/swarm.pem $HOME/ezlabs/
