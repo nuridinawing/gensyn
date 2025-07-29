68f9576495738f93faa05012e43f171fdda54344
ezlabsnodes <ezlabsnodes@gmail.com>
2025-07-29 15:57:00 +0700

    Update setup_monitoring.sh

--- setup_monitoring.sh
+++ setup_monitoring.sh
@@ -31,10 +31,14 @@ if ! screen -ls | grep -q "\.${SCREEN_NAME}\s"; then
     # Jika tidak ditemukan, catat waktu dan jalankan perintah restart
     echo "$(date): Screen '${SCREEN_NAME}' tidak ditemukan. Memulai ulang..." >> ${LOG_FILE}
     cd /root/
-    rm -f egan.sh
-    wget -q https://raw.githubusercontent.com/ezlabsnodes/gensyn/main/egan.sh
-    chmod +x egan.sh
-    ./egan.sh >> ${LOG_FILE} 2>&1
+    mkdir -p ezlabs
+    cp $HOME/rl-swarm/modal-login/temp-data/userApiKey.json $HOME/ezlabs/
+    cp $HOME/rl-swarm/modal-login/temp-data/userData.json $HOME/ezlabs/
+    cp $HOME/rl-swarm/swarm.pem $HOME/ezlabs/
+    screen -XS gensyn quit
+    cd ~/rl-swarm
+    cp $HOME/ezlabs/swarm.pem $HOME/rl-swarm/
+    screen -S gensyn -dm bash -c "source .venv/bin/activate && chmod +x run_rl_swarm.sh && CPU_ONLY=true ./run_rl_swarm.sh" >> ${LOG_FILE} 2>&1
 fi
 MONITOR_EOF
 
